d56bdc60fa87cd05d893195fbe5f7fb834a1059f
MykytaChystyi <neyton61@gmail.com>
2024-09-02 11:03:57 +0300

    Add project files.

---
+++ GrowAcc/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrowAcc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public bool Registration()
+        {
+            return true;
+
+        }
+        public ActionResult Login()
+        {
+            return null;
+        }
+        public ActionResult Logout()
+        {
+            return null;
+        }
+
+    }
+}
---
+++ GrowAcc/Database/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using GrowAcc.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    // Додайте DbSet'и для ваших моделей
+    public DbSet<UserAccount> YourModels { get; set; }
+}
