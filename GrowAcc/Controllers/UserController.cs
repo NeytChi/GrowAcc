590aead3e3f8b94391201ceb77f6ebed6d9d952a
MykytaChystyi <neyton61@gmail.com>
2024-09-16 15:08:04 +0300

    Додав коментар в UserController - треба визначитися, як робити обробку помилок

--- GrowAcc/Controllers/UserController.cs
+++ GrowAcc/Controllers/UserController.cs
@@ -16,13 +16,18 @@ namespace GrowAcc.Controllers
             _userService = userAccountService;
             _userRepository = repository;
         }
+        /// <summary>
+        /// Треба визначитися, яким саме чином опрацьовувати помилки при запиті та при успішному виконанні методу. В якому форматі повертати відповідь.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult Registration([FromBody] UserAccountRegistrationRequest request)
         {
-            var result = _userService.Registration(request);
+            var result = _userService.SingUp(request);
             if (result.IsCompleted)
             {
-                return Ok("User created successfully.");
+                return Ok("User was created successfully.");
             }
             return BadRequest(ModelState);
         }
